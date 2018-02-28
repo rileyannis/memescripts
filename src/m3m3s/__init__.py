b55ff68ea123b9dea77d8f8719a5a0ba03afd951
Jake Fenton <jake@fenton.io>
2018-02-28 11:34:03 -0500

    moved some things, packagified it

---
+++ src/m3m3s/__init__.py
@@ -0,0 +1,27 @@
+import argparse
+from . import decapitator
+
+
+def build_parsers():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    parser.add_argument('--version', help="output m3m3s' version and halt.",
+                        action='version',
+                        version=f"m3m3s {__version__}")
+
+    parser_decap = subparsers.add_parser('decap')
+    decapitator.add_args_to_parser(parser_decap)
+    parser_decap.set_defaults(func=decapitator.main)
+
+    return parser
+
+
+def main():
+    parser = build_parsers()
+    args = parser.parse_args()
+    if hasattr(args, 'func'):
+        args.func(args)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
